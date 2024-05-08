d44fc79b7d811ad319f0851d89957b835f876f8a
Christian Lietz <lietzchristian01@gmail.com>
2024-05-07 19:36:58 -0500

    literally useless, just need more practice

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 # I think I know how to use git now tho.
 #testing git pull and git fetch
 #testing git pull and git fetch pt2
-import numpy
-import pandas
-import matplotlib
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
 
 chris = "que guey"
 
 print(chris)
 print("\nello wurld")
 
-
+#Diego you better be doing something
